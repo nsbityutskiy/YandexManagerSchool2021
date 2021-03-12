45ea8eb9f1376180f05ac73f355f885ecc3819e7
Nikita Bityutskiy <nsbityutskiy@edu.hse.ru>
2021-03-12 16:33:13 +0300

    Теперь задача красивая:)

--- Yandex-Python.py
+++ Yandex-Python.py
@@ -1,12 +1,13 @@
 # нам не сказано, что должна возвращать программа
 # но пускай для удобства она возвращает булевые значения
-def validatepassword(password):
-    # Проверим, что программе подали на ввод строку, а не что-то другое. Ведь если там какое-либо число, пароль всё равно будет неверным.
-    if type(password) == str:
-        # будем проверять пароль пошагово - в мультипоточности смысла нет
-        if len(password) > 6 and sum(i.isdigit() for i in password) >= 1 and sum(i.isupper() for i in password) >= 1 and sum(i.islower() for i in password) >= 1 and password.isalnum() and len(set(password)) == len(password):
-            return True
-        else:
-            return False
+# Проверим, что программе подали на ввод строку, а не что-то другое.
+# Ведь если там какое-либо число, пароль всё равно будет неверным.
+# Будем проверять пароль пошагово - в мультипоточности смысла нет.
+# Начинаем от самого простого условия.
+def valid_password(password):
+    if type(password) == str and len(password) > 6 and sum(i.isdigit() for i in password) >= 1 and sum(
+            i.isupper() for i in password) >= 1 and sum(
+        i.islower() for i in password) >= 1 and password.isalnum() and len(set(password)) == len(password):
+        return True
     else:
         return False
