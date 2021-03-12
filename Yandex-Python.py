c9072f417eb5fd98baf39aa240b105062a684ffe
Nikita Bityutskiy <nsbityutskiy@edu.hse.ru>
2021-03-12 16:27:38 +0300

    Укороченное решение задачи

--- Yandex-Python.py
+++ Yandex-Python.py
@@ -4,26 +4,9 @@ def validatepassword(password):
     # Проверим, что программе подали на ввод строку, а не что-то другое. Ведь если там какое-либо число, пароль всё равно будет неверным.
     if type(password) == str:
         # будем проверять пароль пошагово - в мультипоточности смысла нет
-        if len(password) > 6:
-            if sum(i.isdigit() for i in password) >= 1:
-                if sum(i.isupper() for i in password) >= 1:
-                    if sum(i.islower() for i in password) >= 1:
-                        if password.isalnum():
-                            if len(set(password)) == len(password):
-                                return True
-                            else:
-                                return False
-                        else:
-                            return False
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
+        if len(password) > 6 and sum(i.isdigit() for i in password) >= 1 and sum(i.isupper() for i in password) >= 1 and sum(i.islower() for i in password) >= 1 and password.isalnum() and len(set(password)) == len(password):
+            return True
         else:
             return False
     else:
         return False
-
-    # будем проверять пароль пошагово - в мультипоточности смысла нет
